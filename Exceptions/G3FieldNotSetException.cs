3f02a143d684713c2a18a6bc871074197cce22fc
Michael Puckett <Michael.Puckett@tripleg3.com>
2021-11-23 00:08:34 -0500

    Init project commit with html Blazor alt ComboBox.

---
+++ Exceptions/G3FieldNotSetException.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace G3BlazorComponents.Exceptions
+{
+    public class G3FieldNotSetException : Exception
+    {
+        public G3FieldNotSetException(string fieldName, [CallerLineNumber] int callerLineNumber = 0, [CallerMemberName] string callerMemberName = "")
+        : base($"The field, {fieldName}, was not set, and is required. At {callerMemberName}, line {callerLineNumber}") { }
+    }
+}
---
+++ Exceptions/G3PropertyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace G3BlazorComponents.Exceptions
+{
+    public class G3PropertyNotFoundException<T> : Exception
+    {
+        public G3PropertyNotFoundException(string propertyName, [CallerLineNumber] int callerLineNumber = 0, [CallerMemberName] string callerMemberName = "")
+        : base($"The property, {propertyName}, was not found on {typeof(T).Name}. At {callerMemberName}, line {callerLineNumber}") { }
+    }
+}
