a0a2cc8ec5b4e3323bd3c93b2b207faa37880ad1
Michael Puckett <Michael.Puckett@tripleg3.com>
2021-12-07 23:59:07 -0500

    working on datagrid

---
+++ Records.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace G3BlazorComponents
+{
+    public record Column<T>(Expression<Func<T, object>> PropertyName, string Header);
+
+    internal class Records
+    {
+        public void Test()
+        {
+            var grid = new G3DataGrid<Person>();
+            grid.Columns = new Column<Person>[]
+            {
+                new(x => x.Name, "First Name"),
+                new(x => x.Id, "ID"),
+                new(x => x.Age, "ID"),
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; } 
+        public int Age { get; set; }
+    }
+}
