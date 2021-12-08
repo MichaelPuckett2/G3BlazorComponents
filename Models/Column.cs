55db57e190b98d3241a2d7fe09f72f0483292827
Michael Puckett <Michael.Puckett@tripleg3.com>
2021-12-08 15:26:49 -0500

    Added RowTemplate, FooterTemplate, and uses ItemTemplate in DataGrid.

---
+++ Models/Column.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace G3BlazorComponents
+{
+    public record Column<T>(Expression<Func<T, object>> PropertyName, 
+                            string Header = "",
+                            double Width = double.NaN);
+}
